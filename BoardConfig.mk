0fd87468660a9bc955633d02add2fda5bfebe353
Sultanxda <sultanxda@gmail.com>
2016-07-09 16:38:57 -0700

    Add custom power HAL
    This simple power HAL will configure the "sys.perf.profile" property upon
    receiving performance-profile change requests, so that init can parse
    "sys.perf.profile" and make appropriate changes. This allows for greater
    customization and simplicity when configuring performance profiles.
    
    Supported power modes: powersave, balanced, performance.
    
    Also features the tap-to-wake feature from upstream.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -191,6 +191,8 @@ TARGET_USES_MKE2FS := true
 # Power
 TARGET_HAS_NO_WIFI_STATS := true
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
+TARGET_USES_INTERACTION_BOOST := true
+TARGET_POWERHAL_VARIANT := none
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/recovery/recovery.fstab
