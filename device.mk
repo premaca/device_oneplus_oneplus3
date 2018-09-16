c1378c9b64fe718b8b31627b5c82648bf91c06f9
Alex Cruz <alex@dirtyunicorns.com>
2018-09-16 00:48:15 -0500

    op3: Remove ANT dependency

--- device.mk
+++ device.mk
@@ -176,15 +176,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.vc_call_vol_steps=7 \
     audio_hal.period_multiplier=2
 
-# ANT+
-PRODUCT_PACKAGES += \
-    AntHalService \
-    com.dsi.ant.antradio_library \
-    libantradio
-
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
-
 # Bluetooth
 PRODUCT_PACKAGES += \
     libbt-vendor
