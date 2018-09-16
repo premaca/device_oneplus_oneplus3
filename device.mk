8d027a841df0120efc524319ec41fe9a883893ea
Bruno Martins <bgcngm@gmail.com>
2018-09-16 18:33:36 -0500

    Use QTI bluetooth stack

--- device.mk
+++ device.mk
@@ -186,9 +186,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    libbt-vendor \
-    android.hardware.bluetooth@1.0-service \
-    android.hardware.bluetooth@1.0-impl
+    libbt-vendor
 
 PRODUCT_PROPERTY_OVERRIDES += \
     qcom.bluetooth.soc=rome
