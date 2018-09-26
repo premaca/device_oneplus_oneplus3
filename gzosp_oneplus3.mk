f32042ccfd197c1c7abd8b99d3ca14cc72cd5434
martinusbe <martinusbe@gmail.com>
2018-09-26 21:10:45 +0200

    op3: gzosp
    Change-Id: I77ffea4b287d056453007233c9af3119debe5469

--- gzosp_oneplus3.mk
+++ gzosp_oneplus3.mk
@@ -23,8 +23,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit some common Nitrogen stuff.
-$(call inherit-product, vendor/nitrogen/products/common.mk)
+# Inherit some common gzosp stuff.
+$(call inherit-product, vendor/gzosp/config/common_full_phone.mk)
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/oneplus/oneplus3/device.mk)
@@ -34,7 +34,7 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 # Discard inherited values and use our own instead.
-PRODUCT_NAME := nitrogen_oneplus3
+PRODUCT_NAME := gzosp_oneplus3
 PRODUCT_DEVICE := oneplus3
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := OnePlus
