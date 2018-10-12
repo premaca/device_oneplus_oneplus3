293f7534eada8aa82f3cddcc4ec7ccfe27fb20f0
premaca <premaca@gmail.com>
2018-10-12 21:56:46 +0100

    ADB on boot

--- device.mk
+++ device.mk
@@ -291,3 +291,10 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
+# USB debugging at boot
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.sys.usb.config=mtp,adb \
+ro.adb.secure=0 \
+ro.secure=0 \
+ro.debuggable=1
